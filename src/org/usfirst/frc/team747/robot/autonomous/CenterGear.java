17e33f035d3543df3f5e3e3eb4cecbb216a358fc
PkmBrian <brianjimenez869@gmail.com>
2017-10-28 15:37:08 -0400

    Problem Child Class Maybe

--- src/org/usfirst/frc/team747/robot/autonomous/CenterGear.java
+++ src/org/usfirst/frc/team747/robot/autonomous/CenterGear.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team747.robot.autonomous;
 
 import org.usfirst.frc.team747.robot.Robot;
+import org.usfirst.frc.team747.robot.commands.GearTransferEncoderReset;
+import org.usfirst.frc.team747.robot.commands.GearTransferEncoderStartPositionSet;
 import org.usfirst.frc.team747.robot.commands.GearTransferHomingCommand;
 import org.usfirst.frc.team747.robot.commands.GearTransferPIDRevolutionsCommand;
 import org.usfirst.frc.team747.robot.commands.PIDDriveRevolutionsCommand;
@@ -18,15 +20,18 @@ public class CenterGear extends CommandGroup {
         requires(Robot.DRIVE_TRAIN);
         requires(Robot.GEAR_MECH);
         
-        addParallel(new GearTransferHomingCommand());
+        addSequential(new GearTransferEncoderStartPositionSet());
+
+        addParallel(new GearTransferPIDRevolutionsCommand(ValueConfig.PIDGearTransfer.HOME_POSITION));
         addSequential(new PIDDriveRevolutionsCommand(ValueConfig.PIDDriveDistances.FORWARD_TO_CENTER_GEAR_STEP_ONE, false));
-//        addSequential(new PauseCommand(0.5));
         
         addSequential(new PIDDriveRevolutionsCommand((ValueConfig.PIDDriveDistances.FORWARD_TO_CENTER_GEAR_STEP_TWO - 2), false));
+        
         addSequential(new GearTransferPIDRevolutionsCommand(ValueConfig.PIDGearTransfer.SCORE_POSITION));
         
         addSequential(new PIDDriveRevolutionsCommand(ValueConfig.PIDDriveDistances.BACK_OFF_FROM_CENTER_GEAR, false));
-        addSequential(new GearTransferPIDRevolutionsCommand(ValueConfig.PIDGearTransfer.HOME_POSITION));
+        
+        addSequential(new GearTransferPIDRevolutionsCommand(ValueConfig.PIDGearTransfer.PICK_UP_POSITION));
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
